35cbc924deed7253249504d45931aee7e41ac1f0
praveena <praveenapalla4@gmail.com>
2026-01-15 17:30:06 +0530

    document readme

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "private_ip"{
     value=aws_instance.main.private_ip
     description="private Ip of the instance created"
 }
+
+output " instance_id" {
+    value= aws_instance.main.id
+  
+}
